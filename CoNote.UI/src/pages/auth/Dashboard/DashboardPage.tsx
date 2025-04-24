800b0215fcd0f5f97d9a2f17a74742c5b618848d
Yusuf Mert Güvercin <mertguvercin2942@gmail.com>
2025-04-24 21:47:29 +0300

    temp workspace cards added

--- CoNote.UI/src/pages/auth/Dashboard/DashboardPage.tsx
+++ CoNote.UI/src/pages/auth/Dashboard/DashboardPage.tsx
@@ -6,7 +6,8 @@ import {
 } from "../../../features/workspace/workspaceSlice";
 //components
 import Loading from "../../../components/ui/Loading";
-import { Box } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+import Workspace from "./component/Workspace";
 
 const DashboardPage = () => {
   const workspaces = useSelector(workspaceSelectors.selectAll);
@@ -16,7 +17,42 @@ const DashboardPage = () => {
     return <Loading />;
   }
 
-  return <Box>{workspaces.map((w) => w.name)}</Box>;
+  return (
+    <Stack direction="column" spacing={4}>
+      <Stack direction="column" spacing={2}>
+        <Typography variant="h4">Owned Workspaces</Typography>
+        <Stack direction="row" spacing={2}>
+          {workspaces.map((workspace, index: number) => (
+            <Workspace workspace={workspace} key={index} />
+          ))}
+        </Stack>
+      </Stack>
+      <Stack direction="column" spacing={2}>
+        <Typography variant="h4">Joined Workspaces</Typography>
+        <Stack direction="row" spacing={2}>
+          {workspaces.map((workspace, index: number) => (
+            <Workspace workspace={workspace} key={index} />
+          ))}
+        </Stack>
+      </Stack>
+      <Stack direction="column" spacing={2}>
+        <Typography variant="h4">Last Visited</Typography>
+        <Stack direction="row" spacing={2}>
+          {workspaces.map((workspace, index: number) => (
+            <Workspace workspace={workspace} key={index} />
+          ))}
+        </Stack>
+      </Stack>
+      <Stack direction="column" spacing={2}>
+        <Typography variant="h4">Favorite Workspaces</Typography>
+        <Stack direction="row" spacing={2}>
+          {workspaces.map((workspace, index: number) => (
+            <Workspace workspace={workspace} key={index} />
+          ))}
+        </Stack>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default DashboardPage;
---
+++ CoNote.UI/src/pages/auth/Dashboard/component/Workspace.tsx
@@ -0,0 +1,32 @@
+//models
+import { WorkspaceView } from "../../../../features/workspace/models/WorkspaceView";
+//components
+import { Card, CardMedia, CardContent, Typography } from "@mui/material";
+
+type WorkspacePropTypes = {
+  workspace: WorkspaceView;
+};
+
+const Workspace = ({ workspace }: WorkspacePropTypes) => {
+  return (
+    <Card sx={{ minWidth: 300 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={require("D:/Projeler/CoNote/CoNote.UI/src/assets/images/temp.png")}
+        title="temp image"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {workspace.name}
+        </Typography>
+        {workspace.description && (
+          <Typography variant="body2" sx={{ color: "grey.500" }}>
+            {workspace.description}
+          </Typography>
+        )}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Workspace;
